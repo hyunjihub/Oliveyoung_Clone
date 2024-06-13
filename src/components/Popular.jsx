40a2aabed11a35cccae913b2beb790058719f58b
hyunjihub <syub98774@naver.com>
2024-06-13 20:27:01 +0900

    [fix] 슬라이드 전환 오류 수정

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -70,7 +70,7 @@ const Popular = () => {
             sliderB.current.slickGoTo(0);
             sliderB.current.slickPlay();
           }
-        }, 4000);
+        }, 3000);
       }
     },
   };
@@ -93,7 +93,7 @@ const Popular = () => {
             sliderA.current.slickGoTo(0);
             sliderA.current.slickPlay();
           }
-        }, 4000);
+        }, 3000);
       }
     },
   };
